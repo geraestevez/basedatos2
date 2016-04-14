749567f3a04ce7011bfea180e48d45d25d7fd315
geraestevez <gerardo_271195@hotmail.com>
2016-04-13 19:36:43 -0500

    Update tablas.sql

--- tablas.sql
+++ tablas.sql
@@ -1,29 +1,39 @@
-create  table autor(
-  id_autor integer, 
-  nombre varchar2(100),
-  CONSTRAINT PRIMARY KEY (id_autor));
+Create table libros(
 
+id_libro int auto_increment not null primary key,
 
+nombre varchar(100),
 
-create  table editorial(
-  id_editorial integer, 
-  nombre varchar2(100),
-  CONSTRAINT PRIMARY KEY (id_editorial));
+editorial varchar(100),
 
+autor varchar(100));
 
-create  table libro(
-  id_libro integer, 
-  nombre varchar2(100),
-  drescripcion varchar2(100),
-  id_autor integer,
-  id_editorial integer
-  CONSTRAINT PK_ID_PRODUCTO PRIMARY KEY (id_libro),
-  
-  );
+create table persona(
 
+id_persona int auto_increment not null primary key,
 
+dir varchar(100),
 
-REATE TABLE PRODUCTO (ID_PRODUCTO INTEGER, 
-DESCRIPCION VARCHAR2(120),  MARCA VARCHAR2(120), 
-CATEGORIA VARCHAR2(80), PRECIO FLOAT,
-CONSTRAINT PK_ID_PRODUCTO PRIMARY KEY (ID_PRODUCTO));
+cp int(10),
+
+muni varchar(40),
+
+tel varchar(20),
+
+mail varchar(60),
+
+password varchar(60)
+
+);
+
+create table renta(
+
+id_renta int auto_increment not null primary key,
+
+lid_libro int,
+
+pid_persona int,
+
+FOREIGN KEY (pid_persona) REFERENCES persona(id_persona),
+
+FOREIGN KEY (lid_libro) REFERENCES libros(id_libro));
