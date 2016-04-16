476a3df5b5a3a1033e49ece336d9be54b7d0d43b
geraestevez <gerardo_271195@hotmail.com>
2016-04-16 09:25:41 -0500

    Update tablas.sql

--- tablas.sql
+++ tablas.sql
@@ -1,27 +1,27 @@
 Create table libros(
-id_libro int auto_increment not null,
-nombre varchar(100),
-editorial varchar(100),
-autor varchar(100)
-CONSTRAINT PK1_id_libro PRIMARY KEY (id_libro));
-
-
-create table persona(
-id_persona int auto_increment not null,
-nom varchar(300),
-dir varchar(100),
-cp int(10),
-muni varchar(40),
-tel varchar(20),
-mail varchar(60),
-password varchar(60)
+id_libro integer,
+nombre varchar2(100),
+editorial varchar2(100),
+autor varchar2(100),
+ CONSTRAINT PK_id_libro PRIMARY KEY (id_libro));
+ 
+ create table persona(
+id_persona integer,
+nom varchar2(300),
+dir varchar2(100),
+cp integer,
+muni varchar2(40),
+tel varchar2(20),
+mail varchar2(60),
+password varchar2(60),
 CONSTRAINT PK2_id_persona PRIMARY KEY (id_persona));
 
 
 create table renta(
-id_renta int auto_increment not null,
-lid_libro int,
-pid_persona int,
+id_renta integer,
+lid_libro integer,
+pid_persona integer,
 CONSTRAINT PK3_id_renta PRIMARY KEY (id_renta),
 CONSTRAINT FK1_id_libro FOREIGN KEY (lid_libro) REFERENCES libros(id_libro),
 CONSTRAINT FK2_id_persona FOREIGN KEY (pid_persona) REFERENCES persona(id_persona));
+
