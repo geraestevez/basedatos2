0df1b43a92f348f7036b102e0e075a92e6df4d01
geraestevez <gerardo_271195@hotmail.com>
2016-04-16 09:59:24 -0500

    Update tablas.sql

--- tablas.sql
+++ tablas.sql
@@ -16,7 +16,6 @@ mail varchar2(60),
 password varchar2(60),
 CONSTRAINT PK2_id_persona PRIMARY KEY (id_persona));
 
-
 create table renta(
 id_renta integer,
 lid_libro integer,
@@ -24,6 +23,7 @@ pid_persona integer,
 CONSTRAINT PK3_id_renta PRIMARY KEY (id_renta),
 CONSTRAINT FK1_id_libro FOREIGN KEY (lid_libro) REFERENCES libro(id_libro),
 CONSTRAINT FK2_id_persona FOREIGN KEY (pid_persona) REFERENCES persona(id_persona));
+
 
 
 CREATE SEQUENCE sec_libro
@@ -56,6 +56,37 @@ BEGIN
   select sec_libro.nextval into my_id_libro from dual;
   insert into libro values(my_id_libro, my_nombre, my_editorial,my_autor);
 END guardar_libro;
+/
+
+create or replace 
+PROCEDURE guardar_persona
+(
+my_id_persona out varchar2,
+my_nom in varchar2,
+my_dir in varchar2,
+my_cp in varchar2,
+my_muni in varchar2,
+my_tel in varchar2,
+my_mail in varchar2,
+my_password in varchar2
+) AS 
+BEGIN
+  select sec_persona.nextval into my_id_persona from dual;
+  insert into persona values(my_id_persona, my_nom, my_dir,my_cp,my_muni,my_tel,my_mail,my_password);
+END guardar_persona;
+/
+
+create or replace 
+PROCEDURE guardar_renta
+(
+my_id_renta out varchar2,
+my_lid_libro in varchar2,
+my_pid_persona in varchar2
+) AS 
+BEGIN
+  select sec_renta.nextval into my_id_renta from dual;
+  insert into renta values(my_id_renta, my_lid_libro, my_pid_persona);
+END guardar_renta;
 /
 
 
