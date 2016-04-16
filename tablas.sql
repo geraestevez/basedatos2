d043e546a84c0397d9d1ff5bca8cdd4b70abb10b
geraestevez <gerardo_271195@hotmail.com>
2016-04-16 09:46:54 -0500

    Update tablas.sql

--- tablas.sql
+++ tablas.sql
@@ -1,4 +1,4 @@
-Create table libros(
+Create table libro(
 id_libro integer,
 nombre varchar2(100),
 editorial varchar2(100),
@@ -22,6 +22,41 @@ id_renta integer,
 lid_libro integer,
 pid_persona integer,
 CONSTRAINT PK3_id_renta PRIMARY KEY (id_renta),
-CONSTRAINT FK1_id_libro FOREIGN KEY (lid_libro) REFERENCES libros(id_libro),
+CONSTRAINT FK1_id_libro FOREIGN KEY (lid_libro) REFERENCES libro(id_libro),
 CONSTRAINT FK2_id_persona FOREIGN KEY (pid_persona) REFERENCES persona(id_persona));
+
+
+CREATE SEQUENCE sec_libro
+start WITH 1
+increment by 1
+nomaxvalue;
+
+CREATE SEQUENCE sec_persona
+start WITH 1
+increment by 1
+nomaxvalue;
+
+CREATE SEQUENCE sec_renta
+start WITH 1
+increment by 1
+nomaxvalue;
+
+
+
+
+create or replace 
+PROCEDURE guardar_libro
+(
+my_id_libro out varchar2,
+my_nombre in varchar2,
+my_editorial in varchar2,
+my_autor in varchar2
+) AS 
+BEGIN
+  select sec_libro.nextval into my_id_libro from dual;
+  insert into libro values(my_id_libro, my_nombre, my_editorial,my_autor);
+END guardar_libro;
+/
+
+
 
